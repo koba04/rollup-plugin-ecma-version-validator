c09f05c7e374c6c061e5fe0de8c415af09922b8d
Nguyen Thai Hung <59815499+hung-cybo@users.noreply.github.com>
2023-08-14 00:04:01 +0900

    chore: add rollup 3x to peerDeps (#39)
    * chore: add rollup 3x to peerDeps
    
    * chore: upgrade rollup devDeps to version 3x
    
    * chore: fix typo error
    
    * chore: remove redundant import
    
    * fix: adding wait

--- src/__tests__/format.test.ts
+++ src/__tests__/format.test.ts
@@ -4,7 +4,7 @@ describe("format", () => {
   it("should return a formatted message", () => {
     const source = "var a = true;\nconst foo = () => {}";
     const error = {
-      message: "const is a reverved word",
+      message: "const is a reserved word",
       loc: {
         line: 2,
         column: 0,
@@ -15,14 +15,14 @@ describe("format", () => {
   it("should return a formatted message if loc is not defined", () => {
     const source = "var a = true;\nconst foo = () => {}";
     const error = {
-      message: "const is a reverved word",
+      message: "const is a reserved word",
     };
     expect(format(error, source)).toMatchSnapshot();
   });
   it("should omit a source code if the error line is over 100 characters", () => {
     const source = `var ${"a".repeat(100)} = () => {}; // ${"b".repeat(200)}`;
     const error = {
-      message: "const is a reverved word",
+      message: "const is a reserved word",
       loc: {
         line: 1,
         column: 110,
